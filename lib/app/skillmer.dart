b3575c2be0a4390ff58ff0b6392664c658135c98
dtulumidis <dextavision@gmail.com>
2021-06-05 23:05:58 +0200

    added amplify cognito authentication basic setup, working with google sign in

--- lib/app/skillmer.dart
+++ lib/app/skillmer.dart
@@ -1,10 +1,35 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 import 'package:skillmer/app/theme.dart';
 import 'package:skillmer/views/navigation/navigation.dart';
 import 'package:skillmer/views/profile/profile.dart';
 import 'package:skillmer/views/profile/profile_settings.dart';
 
-class Skillmer extends StatelessWidget {
+import '../amplifyconfiguration.dart';
+
+class Skillmer extends StatefulWidget {
+  @override
+  _SkillmerState createState() => _SkillmerState();
+}
+
+class _SkillmerState extends State<Skillmer> {
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+    Amplify.addPlugins([authPlugin]);
+
+    try {
+      await Amplify.configure(amplifyconfig);
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +39,7 @@ class Skillmer extends StatelessWidget {
       routes: {
         '/': (context) => Navigation(),
         '/profile': (context) => Profile(),
-        '/profile-settings': (context) => ProfileSettings()
+        '/profile-settings': (context) => ProfileSettings(),
       },
     );
   }
