44b8805987c2393c7261a1f433d11dd88d4f6fd5
dtulumidis <dextavision@gmail.com>
2021-08-22 17:47:07 +0200

    designing comment section for posts

--- lib/views/user_post/widgets/comment.dart
+++ lib/views/user_post/widgets/comment.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Comment extends StatelessWidget {
+class Comment extends ConsumerWidget {
   const Comment();
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('sdf'),
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 1.0,
+                ),
+                child: Container(
+                  width: 20.0,
+                  height: 25.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/Skillmer.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: RichText(
+                  text: new TextSpan(
+                    // Note: Styles for TextSpans must be explicitly defined.
+                    // Child text spans will inherit styles from parent
+                    style: new TextStyle(
+                      fontSize: 14.0,
+                    ),
+                    children: <TextSpan>[
+                      new TextSpan(
+                          text: 'Dextavision ',
+                          style: new TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: 'Voll der mega krasse Comment, beste App ever!',
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 25.0,
+              ),
+              IconButton(
+                icon: Icon(Icons.plus_one_outlined),
+                onPressed: () {},
+                iconSize: 20.0,
+                color: Colors.white,
+              ),
+              SizedBox(width: 20.0),
+              Text('Answer'),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
